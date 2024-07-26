a3264408069683ce8ba03cba592b840b682e16d1
Vinayak Aggarwal <vinayakaggarwal05@gmail.com>
2024-07-26 21:22:10 +0530

    base setup

---
+++ src/main/java/com/vinayak/flight_status/FlightStatusApplication.java
@@ -0,0 +1,13 @@
+package com.vinayak.flight_status;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightStatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinayak/flight_status/FlightStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinayak.flight_status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
